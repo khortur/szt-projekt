b9339986d23f6c0fcd52cdf2dede6faf7023335b
biernacki <khortur@gmail.com>
2017-01-22 17:05:26 +0100

    Add views to add and edit products

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.item_list, name='item_list'),
     url(r'^item/(?P<pk>[0-9]+)/$', views.item_detail, name='item_detail'),
+    url(r'^item/new/$', views.item_new, name='item_new'),
+    url(r'^item/(?P<pk>[0-9]+)/edit/$', views.item_edit, name='item_edit'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Item
+from .forms import ItemForm
 
 def item_list(request):
     # items = Item.objects.filter(stock__gt=0).order_by('price')
@@ -9,3 +10,26 @@ def item_list(request):
 def item_detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     return render(request, 'store/item_detail.html', {'item': item})
+
+def item_new(request):
+    if request.method == "POST":
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.save()
+            return redirect('item_detail', pk=item.pk)
+    else:
+        form = ItemForm()
+    return render(request, 'store/item_edit.html', {'form': form})
+
+def item_edit(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    if request.method == "POST":
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.save()
+            return redirect('item_detail', pk=item.pk)
+    else:
+        form = ItemForm(instance=item)
+    return render(request, 'store/item_edit.html', {'form': form})
