ef1f819aadfcc80b9b8629b3e41d3376bd8795eb
biernacki <khortur@gmail.com>
2017-01-22 19:50:02 +0100

    Add possibility to buy products

--- store/models.py
+++ store/models.py
@@ -12,3 +12,14 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+class Purchase(models.Model):
+    item = models.ForeignKey('store.Item', related_name='purchases')
+    buyer = models.ForeignKey('auth.User')
+    amount = models.IntegerField(default=0, null=True)
+
+    def total_price(self):
+        return self.amount * self.item.price
+
+    def __str__(self):
+        return self.item.name + " : " + self.buyer.username + " : " + self.amount.__str__()
--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Item
+from .models import Item, Purchase
 from .forms import ItemForm
 from django.contrib.auth.decorators import login_required
 
@@ -42,3 +42,42 @@ def item_remove(request, pk):
     item = get_object_or_404(Item, pk=pk)
     item.delete()
     return redirect('item_list')
+
+@login_required
+def cart(request):
+    user = request.user
+    purchases = Purchase.objects.filter(buyer_id__exact=user.id)
+    return render(request, 'store/cart.html', {"purchases": purchases})
+
+@login_required
+def purchase_remove(request, pk):
+    purchase = get_object_or_404(Purchase, pk=pk)
+    purchase.delete()
+    return redirect('cart')
+
+@login_required
+def purchase_add(request, item_id, amount):
+    item = get_object_or_404(Item, pk=item_id)
+    user = request.user
+    purchases = Purchase.objects.filter(item_id__exact=item_id).filter(buyer_id__exact=user.id)
+    purchase = Purchase(item=item, amount=amount, buyer=user)
+    if (purchases.count() > 0):
+        purchase = purchases.first()
+        purchase.amount += int(amount)
+        if (purchase.amount > item.stock):
+            purchase.amount = item.stock
+    purchase.save()
+    return redirect('cart')
+
+@login_required
+def accept_cart(request):
+    user = request.user
+    purchases = Purchase.objects.filter(buyer_id__exact=user.id)
+    for purchase in purchases:
+        item = purchase.item
+        item.stock -= purchase.amount
+        if item.stock == 0:
+            item.delete()
+        else:
+            item.save()
+    return render(request, 'store/accept_cart.html', {})
